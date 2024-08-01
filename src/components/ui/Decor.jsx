b11f3f570aae1e6ec2dd5af97227a733a8b08986
AngelicaTrumbower <a_m_hello@yahoo.com>
2024-08-01 17:25:24 -0400

    fix style

--- src/components/ui/Decor.jsx
+++ src/components/ui/Decor.jsx
@@ -4,58 +4,55 @@ import Rating from "./Rating";
 import Price from "./Price";
 
 const Decor = ({ decor }) => {
-    const [img, setImg] = useState();
+  const [img, setImg] = useState();
 
-    const mountedRef = useRef(true);
+  const mountedRef = useRef(true);
 
-    useEffect(() => {
-        const image = new Image();
-        image.src = decor.url;
-        image.onload =() => {
-            setTimeout(() => {
-                if (mountedRef.current) {
-                    setImg(image);
-                }
-            }, 300);
-        };
-        return () => {
-            mountedRef.current = false;
+  useEffect(() => {
+    const image = new Image();
+    image.src = decor.url;
+    image.onload = () => {
+      setTimeout(() => {
+        if (mountedRef.current) {
+          setImg(image);
         }
-    }, [decor.url])
+      }, 30);
+    };
+    return () => {
+      mountedRef.current = false;
+    };
+  }, [decor.url]);
 
-    return (
-        <div className="decor">
-            {img ? (
-                <>
+  return (
+    <div className="decor">
+      {!img ? (
+            <>
+                <div className="decor__img--skeleton"></div>
+                <div className="decor__title--skeleton skeleton"></div>
+                <div className="decor__rating--skeleton skeleton"></div>
+                <div className="decor__price--skeleton skeleton"></div>
+            </>
+        ) : (
+            <>
                 <Link to={`/decors/${decor.id}`}>
-                        <figure className="decor__img__wrapper">
-                        <img
-                            src={img.src}
-                            alt=""
-                            className="decor__img"
-                        />
-                        </figure>
-                    </Link>
-                    <div className="decor__title">
-                        <Link to={`/decors/${decor.id}`} className="decor__title--link">
+                    <figure className="decor__img__wrapper">
+                    <img src={img.src} alt="" className="decor__img" />
+                    </figure>
+                </Link>
+                <div className="decor__title">
+                    <Link to={`/decors/${decor.id}`} className="decor__title--link">
                         {decor.title}
-                        </Link>
-                    </div>
-                    <Rating rating={decor.rating} />
-                    <Price
-                        originalPrice={decor.originalPrice}
-                        salePrice={decor.salePrice}/>{" "} 
-                </>
-                ) : (
-                <>
-                    <div className="decor__img--skeleton"></div>
-                    <div className="decor__title--skeleton skeleton"></div>
-                    <div className="decor__rating--skeleton skeleton"></div>
-                    <div className="decor__price--skeleton skeleton"></div>
-                </> )
-            };
-        </div>
-    );
+                    </Link>
+                </div>
+                <Rating rating={decor.rating} />
+                <Price
+                    originalPrice={decor.originalPrice}
+                    salePrice={decor.salePrice}
+                />
+            </>
+        )}
+    </div>
+  );
 };
 
 export default Decor;
